5d8f37dacf335c965ef4479101920e3bcf72ff96
OneJ <3942241@qq.com>
2017-04-27 17:37:44 +0800

    +

---
+++ WPFDemo/RoutedEvent/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvent
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am the outer button");
+        }
+
+        private void OuterEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("I am the green ellipse");
+        }
+
+        private void InnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I am the Inner ellipse");
+        }
+    }
+}
