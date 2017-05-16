0d9268ab8e0efe2a72ddd4f13256f041575bf041
OneJ <3942241@qq.com>
2017-05-16 18:08:58 +0800

    +

---
+++ WPFDemo/ADO.Net/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADO.Net
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source = .;Initial Catalog = demo_test;User Id = sa;Password = kingwangboss;"))
+            {
+                connection.Open();
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "insert into Student(Stu_name,Stu_desc) values('vxcvxcv','rteye');";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("执行完成");
+        }
+    }
+}
